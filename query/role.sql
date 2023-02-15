8717bd7dec37354a7cd772d37228aa42f4386bc7
coderXomDuong5 <Thckpck11@gmail.com>
2023-02-15 10:19:18 +0700

    Update trigger

--- query/role.sql
+++ query/role.sql
@@ -1,19 +1,18 @@
-revoke create on schema public from public;
-revoke all on database final_project from public;
+grant create on schema public to public;
+revoke all on database DS2_database from public;
 
-drop role app_readonly;
-create role app_readonly;
+drop role if exists viewer;
+create role viewer;
+grant connect on database library to viewer;
 
-grant connect on database final_project to app_readonly;
-create schema app_schema;
-grant usage on schema app_schema to app_readonly;
-grant select on all tables in schema app_schema to app_readonly;
-alter default privileges in schema app_schema grant select on tables to app_readonly;
+grant usage on schema public to viewer;
+grant select on all tables in schema public to viewer;
+alter default privileges in schema public grant select on tables to viewer;
 
-drop role read_write;
-create role read_write;
+drop role if exists customer;
+create role customer;
 
-grant connect on database final_project to read_write;
+grant connect on database public to read_write;
 grant usage, create on schema app_schema to read_write;
 grant select, insert, update, delete on all tables in schema app_schema to read_write;
 alter default privileges in schema app_schema grant select, insert, update, delete on tables to read_write;
@@ -27,10 +26,12 @@ create user app_reporting_user2 with password 'password2';
 create user app_user1 with password 'password3';
 create user app_user2 with password 'password4';
 
-grant app_readonly to app_reporting_user1;
-grant app_readonly to app_reporting_user2;
+grant viewer to app_reporting_user1;
+grant viewer to app_reporting_user2;
 grant read_write to app_user1;
 grant read_write to app_user2;
 
+select * from club
+order by 2;
 
 
